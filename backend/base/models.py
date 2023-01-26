d79e2f1fcd634019aa01f7eaa1c4a75663548143
Oizys13 <brchred@gmail.com>
2023-01-26 18:12:20 +0100

    user field updated

--- backend/base/models.py
+++ backend/base/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     phone = models.IntegerField(null=True, blank=True)
-    Nationality = models.IntegerField(null=True, blank=True)
+    Nationality = models.CharField(null=True, blank=True)
     def __str__(self):
         return self.username
